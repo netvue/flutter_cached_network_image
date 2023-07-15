21a153bf653ecd8275d9490ead488aa95c4bba8c
Panqing Huang <panqing.huang@netvue.com>
2023-07-15 17:08:27 +0800

    fix: comment out rethrow to avoid crashing when the image is inaccessible.

--- cached_network_image/lib/src/image_provider/_image_loader.dart
+++ cached_network_image/lib/src/image_provider/_image_loader.dart
@@ -131,7 +131,7 @@ class ImageLoader implements platform.ImageLoader {
       });
 
       errorListener?.call();
-      rethrow;
+      // rethrow;
     } finally {
       await chunkEvents.close();
     }
